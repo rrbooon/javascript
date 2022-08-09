4cabc605fba05a926920f1a3da53fc51e36a5b74
rrbooon <rogerio.colla58@gmail.com>
2022-08-09 10:18:40 -0300

    giving_the_right_name

--- 2_variables/1_working_with_variables/working_with_variables.js
+++ 2_variables/1_working_with_variables/working_with_variables.js
@@ -1,3 +1,10 @@
+/*
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).
+*/
+
 let name = "John";
 let admin = name;
 
